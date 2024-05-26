0f3430ce5cdde4d025f47d9df7200091dfe6ccca
Jinmiao Luo <39730824+jinmiaoluo@users.noreply.github.com>
2024-05-26 22:47:42 +0800

    Add GPU support for OpenAI Whisper model

--- src/asr/whisper_asr.py
+++ src/asr/whisper_asr.py
@@ -1,4 +1,5 @@
 import os
+import torch
 
 from transformers import pipeline
 
@@ -9,9 +10,12 @@ from .asr_interface import ASRInterface
 
 class WhisperASR(ASRInterface):
     def __init__(self, **kwargs):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
         model_name = kwargs.get("model_name", "openai/whisper-large-v3")
         self.asr_pipeline = pipeline(
-            "automatic-speech-recognition", model=model_name
+            "automatic-speech-recognition",
+            model=model_name,
+            device=device,
         )
 
     async def transcribe(self, client):
