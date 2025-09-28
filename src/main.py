ab1236ebfe32764799019a44fa8e0b4baa7c3bc6
Justyn Leung <justynleung2000@gmail.com>
2025-09-28 10:25:07 -0400

    Fix RuntimeError: no running event loop

--- src/main.py
+++ src/main.py
@@ -70,7 +70,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def main():
+async def main():
     args = parse_args()
 
     logging.basicConfig()
@@ -97,9 +97,9 @@ def main():
         keyfile=args.keyfile,
     )
 
-    asyncio.get_event_loop().run_until_complete(server.start())
-    asyncio.get_event_loop().run_forever()
+    await server.start()
+    await asyncio.Future()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
